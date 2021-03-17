ee623c5f93d853237d915d92dcf1cb57ac7f35e1
Laimonas Masionis <71811684+LaimonasMas@users.noreply.github.com>
2021-03-17 17:16:31 +0200

    format fix

--- views/tableMainRow.php
+++ views/tableMainRow.php
@@ -3,4 +3,4 @@
 <td><?= $account->surname ?></td>
 <td><?= $account->personalID ?></td>
 <td><?= $account->accountNumber ?></td>
-<td>€ <?= $account->amount ?> ¥ or GBP <?= $amount = (App\Json::getRate('GBP') * $account->amount) ?></td>
+<td>€ <?= $account->amount ?><br>¥ <?= $amount = (App\Json::getRate('JPY') * $account->amount) ?></td>
